51871a67b91df2d586bd0d6abf0472bafb166149
mahirlabib478-del <mahirlabib478@gmail.com>
2026-09-25 00:21:53 +0600

    Copy new-app file: app/src/main/kotlin/com/aistudio/studyos/data/update/AppUpdateModel.kt

---
+++ app/src/main/kotlin/com/aistudio/studyos/data/update/AppUpdateModel.kt
@@ -0,0 +1,31 @@
+package com.aistudio.studyos.data.update
+
+data class AppUpdateInfo(
+    val latestVersion: String,
+    val minimumSupportedVersion: String = "0.0.0",
+    val releaseUrl: String,
+    val apkUrl: String,
+    val releaseNotes: String = "",
+    val isMandatory: Boolean = false
+)
+
+sealed class UpdateDownloadState {
+    object Idle : UpdateDownloadState()
+    data class Downloading(val progressPercent: Int, val downloadedBytes: Long, val totalBytes: Long) : UpdateDownloadState()
+    object ReadyToInstall : UpdateDownloadState()
+    data class Error(val message: String) : UpdateDownloadState()
+}
+
+sealed class UpdateCheckState {
+    object Idle : UpdateCheckState()
+    object Checking : UpdateCheckState()
+    data class Available(val updateInfo: AppUpdateInfo) : UpdateCheckState()
+    data class UpToDate(val currentVersion: String) : UpdateCheckState()
+    data class Error(val message: String) : UpdateCheckState()
+}
+
+sealed class UpdateCheckResult {
+    data class Available(val updateInfo: AppUpdateInfo) : UpdateCheckResult()
+    data class UpToDate(val currentVersion: String) : UpdateCheckResult()
+    data class Error(val message: String) : UpdateCheckResult()
+}
